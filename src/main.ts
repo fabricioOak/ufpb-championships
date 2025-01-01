691e8069436709931c453c74d4a43d0d50992105
Fabricio Carvalho <fcarvalho1999@gmail.com>
2025-01-01 17:49:27 -0300

    remove unised icon package

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { OhVueIcon, addIcons } from 'oh-vue-icons'
-import { CoTwitch, IoGameController, RiTeamFill, CoNotes } from 'oh-vue-icons/icons'
-
-addIcons(CoTwitch, IoGameController, RiTeamFill, CoNotes)
 
 const app = createApp(App)
-
-app.component('v-icon', OhVueIcon)
 
 app.use(router)
 
